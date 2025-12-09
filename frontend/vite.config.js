946e94495b739d2439295d1728d31ee66c2c11c6
Mateo <mateo@gmail.com>
2025-12-09 13:52:28 -0500

    Fix: Vite config para GitHub Pages y reintento de Docker Push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ¡Añade esta línea! Es la que soluciona la pantalla en blanco en GitHub Pages.
+  base: '/mi-proyecto-ci-cd-express-vite/',
 })
